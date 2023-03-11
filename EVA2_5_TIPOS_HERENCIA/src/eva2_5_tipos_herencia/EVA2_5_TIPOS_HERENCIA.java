c2daf0fa0a2f72322cdd106c2651579aa762a23a
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2023-03-11 15:56:17 -0600

    Add files via upload

---
+++ EVA2_5_TIPOS_HERENCIA/src/eva2_5_tipos_herencia/EVA2_5_TIPOS_HERENCIA.java
@@ -0,0 +1,21 @@
+package eva2_5_tipos_herencia;
+
+public class EVA2_5_TIPOS_HERENCIA {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+class SuperClase{
+    
+}
+
+class OtraSuperClase{ //No se puede heredar de dos o mas superclases
+    
+}
+                        //extends solo funciona para una unica clase
+class BaseClase extends SuperClase{
+    
+}
+}
