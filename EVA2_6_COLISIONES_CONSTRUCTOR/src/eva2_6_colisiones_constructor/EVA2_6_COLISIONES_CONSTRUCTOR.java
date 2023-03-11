a7daa849ad915bafc335e630370710756cfa49ca
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2023-03-11 16:04:18 -0600

    Add files via upload

---
+++ EVA2_6_COLISIONES_CONSTRUCTOR/src/eva2_6_colisiones_constructor/EVA2_6_COLISIONES_CONSTRUCTOR.java
@@ -0,0 +1,17 @@
+package eva2_6_colisiones_constructor;
+
+public class EVA2_6_COLISIONES_CONSTRUCTOR {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Animal animal = new Animal();
+        animal.comer();
+        animal.respirar();
+        
+        System.out.println("Mamifero");
+        Mamiferos mamifero = new Mamiferos();
+
+    }
+    
+}
---
+++ EVA2_6_COLISIONES_CONSTRUCTOR/src/eva2_6_colisiones_constructor/Mamiferos.java
@@ -0,0 +1,16 @@
+package eva2_6_colisiones_constructor;
+
+public class Mamiferos extends Animal{
+
+    public Mamiferos() {
+        System.out.println("Soy un mamifero");
+    }
+        
+    public void parir(){
+        System.out.println("No nacemos de huevo");
+    }
+    
+    public void pelo(){
+        System.out.println("Tenemos pelo");
+    }
+}
