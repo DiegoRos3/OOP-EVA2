f7b6d8469b39248111ec776411d3cfb2e105f401
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2023-03-20 17:24:48 -0600

    Add files via upload

---
+++ EVA2_13_FIGURAS_ABSTRACTAS/src/eva2_13_figuras_abstractas/EVA2_13_FIGURAS_ABSTRACTAS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_13_figuras_abstractas;
+
+/**
+ *
+ * @author Diego
+ */
+public class EVA2_13_FIGURAS_ABSTRACTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Figura fig = new Figura(); no se pude crear objeto de figura (es abstracta)
+        Circulo circ = new Circulo();
+        Triangulo tria = new Triangulo();
+        
+    }
+    
+}
---
+++ EVA2_13_FIGURAS_ABSTRACTAS/src/eva2_13_figuras_abstractas/Figura.java
@@ -0,0 +1,9 @@
+package eva2_13_figuras_abstractas;
+
+abstract public class Figura {
+    
+    public abstract double calcularArea();
+    
+    public abstract double calcularPeri();
+    
+}
