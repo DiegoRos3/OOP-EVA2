ad35e0ff53e76fc8071aaed38a649f4bbf5c8f72
Diego Rose <124701925+DiegoRos3@users.noreply.github.com>
2023-03-26 20:02:18 -0600

    Add files via upload

---
+++ EVA2_18_INTERFACES_HERENCIA/src/eva2_18_interfaces_herencia/IntC.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package eva2_18_interfaces_herencia;
+
+/**
+ *
+ * @author Diego
+ */
+public interface IntC extends IntA, IntB{
+    void C();
+}
---
+++ EVA2_18_INTERFACES_HERENCIA/src/eva2_18_interfaces_herencia/Prueba.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva2_18_interfaces_herencia;
+
+/**
+ *
+ * @author Diego
+ */
+public class Prueba implements IntC{
+
+    @Override
+    public void C() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void A() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void B() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
